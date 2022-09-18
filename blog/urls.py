d76b4b99057041b52cd2c52cca059ae0cee34a56
Danial-Azarpanah <danial.azarpanah.dev@gmail.com>
2022-09-18 08:17:57 +0430

    Added obtain_auth_token view for authentication via token

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+
+from rest_framework.authtoken import views as token_views
+
 from . import views
 
 urlpatterns = [
@@ -10,4 +13,5 @@ urlpatterns = [
     path('articles/add', views.AddArticleView.as_view()),
     path('articles/update/<int:pk>', views.UpdateArticleView.as_view()),
     path('check', views.CheckToken.as_view()),
+    path('login', token_views.obtain_auth_token),
 ]
